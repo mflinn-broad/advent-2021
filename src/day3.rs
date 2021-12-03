009c26b33db618e736730551d8b41c5f4dba67b1
mflinn-broad <mflinn@broadinstitute.org>
2021-12-03 10:48:02 -0500

    asdf

---
+++ src/day3.rs
@@ -0,0 +1,13 @@
+use std::{fs::File, io::{Error, Read}};
+
+fn read_input(path: &str) -> Result<String, Error> {
+    let mut file_handle = File::open(path)?;
+    let mut content = String::new();
+    file_handle.read_to_string(&mut content)?;
+    Ok(content)
+}
+
+
+pub fn run() {
+    println!("...");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use advent_2021::{day1, day2};
+use advent_2021::*;
 
 fn main() {
     println!("Day 1 --------");
     day1::run();
     println!("Day 2 --------");
     day2::run();
+    println!("Day 3 -------");
+    day3::run();
 }
