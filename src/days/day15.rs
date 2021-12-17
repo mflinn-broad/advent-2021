6a3500c5c556ffe4e81dc37d19a1cb52a69d7303
mflinn-broad <mflinn@broadinstitute.org>
2021-12-17 11:33:36 -0500

    day15

--- src/days/day15.rs
+++ src/days/day15.rs
@@ -16,22 +16,24 @@ fn part_1(input: Vec<Vec<i32>>) -> i32 {
 
     dijkstra::dijkstra(
         &start,
-        |(x, y)| {
-            SURROUNDING_POINTS
-                .iter()
-                .map(|(x_move, y_move)| {
-                    input
-                        .get((x + x_move) as usize)
-                        .and_then(|row| row.get((y + y_move) as usize))
-                        .map(|risk| ((x + x_move, y + y_move), *risk))
-                })
-                .flatten()
-                .collect::<Vec<_>>()
-        },
+        |(x, y)| successors_small((x, y), &input),
         |&node| node == goal,
     )
     .unwrap()
     .1
+}
+
+fn successors_small(current: (&i32, &i32), grid: &[Vec<i32>]) -> Vec<((i32, i32), i32)> {
+    let (x, y) = current;
+    SURROUNDING_POINTS
+        .iter()
+        .map(|(x_move, y_move)| {
+            grid.get((x + x_move) as usize)
+                .and_then(|row| row.get((y + y_move) as usize))
+                .map(|risk| ((x + x_move, y + y_move), *risk))
+        })
+        .flatten()
+        .collect::<Vec<_>>()
 }
 
 fn part_2(input: Vec<Vec<i32>>) -> i32 {
@@ -41,30 +43,33 @@ fn part_2(input: Vec<Vec<i32>>) -> i32 {
 
     dijkstra::dijkstra(
         &start,
-        |(x, y)| {
-            SURROUNDING_POINTS
-                .iter()
-                .map(|(x_move, y_move)| ((x + x_move) as usize, (y + y_move) as usize))
-                .filter(|(x, y)| (x / 5 < square_size && y / 5 < square_size))
-                .map(|(x, y)| {
-                    input
-                        .get(x % square_size)
-                        .and_then(|row| row.get(y % square_size))
-                        .map(|risk| {
-                            (
-                                (x as i32, y as i32),
-                                ((*risk as usize + (x / square_size) + (y / square_size) - 1) % 9
-                                    + 1) as i32,
-                            )
-                        })
-                })
-                .flatten()
-                .collect::<Vec<_>>()
-        },
+        |(x, y)| successors_large((x, y), &input),
         |&node| node == goal,
     )
     .unwrap()
     .1
+}
+
+fn successors_large(current: (&i32, &i32), grid: &[Vec<i32>]) -> Vec<((i32, i32), i32)> {
+    let (x, y) = current;
+    let square_size = grid.len();
+    SURROUNDING_POINTS
+        .iter()
+        .map(|(x_move, y_move)| ((x + x_move) as usize, (y + y_move) as usize))
+        .filter(|(x, y)| (x / 5 < square_size && y / 5 < square_size))
+        .map(|(x, y)| {
+            grid.get(x % square_size)
+                .and_then(|row| row.get(y % square_size))
+                .map(|risk| {
+                    (
+                        (x as i32, y as i32),
+                        ((*risk as usize + (x / square_size) + (y / square_size) - 1) % 9 + 1)
+                            as i32,
+                    )
+                })
+        })
+        .flatten()
+        .collect::<Vec<_>>()
 }
 
 fn process(input: &str) -> Vec<Vec<i32>> {
@@ -77,4 +82,29 @@ fn process(input: &str) -> Vec<Vec<i32>> {
                 .collect()
         })
         .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    extern crate test;
+    use test::Bencher;
+
+    #[bench]
+    fn bench_part_1(b: &mut Bencher) {
+        let raw_input = util::read_input("inputs/day15.txt").unwrap();
+        b.iter(|| {
+            let input = process(&raw_input);
+            part_1(input);
+        });
+    }
+
+    #[bench]
+    fn bench_part_2(b: &mut Bencher) {
+        let raw_input = util::read_input("inputs/day15.txt").unwrap();
+        b.iter(|| {
+            let input = process(&raw_input);
+            part_2(input);
+        });
+    }
 }
